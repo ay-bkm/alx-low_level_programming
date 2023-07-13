6b8f866ca007dcf6e2b59b0154b16a5c7081d41a
Ayoub <ay.belkacem.gmail.com>
2023-07-13 09:07:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /*
  * Description : Prints a string to the standard output
  */
-int main(void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
