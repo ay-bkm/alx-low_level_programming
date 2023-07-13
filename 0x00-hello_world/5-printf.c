c7f48495e283502a80b04c2f87d38068bb093ed6
Ayoub <ay.belkacem.gmail.com>
2023-07-13 09:31:58 -0700

    Printf, prints string to standard output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Main - Entry Point
+ *
+ * Description: prints a string to standard output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
