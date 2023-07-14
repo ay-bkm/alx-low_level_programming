47b89429154a2c0416032876085936255e8242c7
Ayoub <ay.belkacem.gmail.com>
2023-07-14 16:58:50 -0700

    C variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+*main - Entry Point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+putchar('H');
+putchar('e');
+putchar('l');
+putchar('l');
+putchar('o');
+putchar(',');
+putchar(' ');
+putchar('W');
+putchar('o');
+putchar('r');
+putchar('l');
+putchar('d');
+putchar('!');
+putchar('\n');
+return(0);
+}
