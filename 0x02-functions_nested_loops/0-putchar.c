5fee34f55d96cdec0bce25c5540ffdabe8035b36
Ayoub <ay.belkacem.gmail.com>
2023-07-18 09:15:23 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,16 @@
  * main - prints "_putchar"
  * Return: 0 Always (Success)
  */
- int main(void) 
- {
-     _putchar('_');
-     _putchar('p');
-     _putchar('u');
-     _putchar('t');
-     _putchar('c');
-     _putchar('h');
-     _putchar('a');
-     _putchar('r');
-     _putchar('\n');
-     return 0;
- }
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
