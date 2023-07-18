32149076b01c5f77fc43c2ac7cd9e7faf78f9cb1
Ayoub <ay.belkacem.gmail.com>
2023-07-18 10:21:04 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@ void print_alphabet_x10(void)
 {
 char letter;
 int i;
-while (i <= 10)
+for (i = 0; i < 10; i++)
 {
-i++;
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
 }
+_putchar('\n');
 }
 }
