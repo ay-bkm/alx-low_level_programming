86fb6e8a26092b8bcc0ac4ce1209a0e8accad3e6
Ayoub <ay.belkacem.gmail.com>
2023-07-18 17:34:31 -0700

    advanced task - 100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+* print_times_table - prints the n times table, starting with 0
+* @n: number of the times table
+*/
+void print_times_table(int n)
+{
+int i, j;
+
+if (n < 0 || n > 15)
+return;
+
+for (i = 0; i <= n; i++)
+{
+_putchar('0');
+for (j = 1; j <= n; j++)
+{
+_putchar(',');
+_putchar(' ');
+if (i * j < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(i * j + '0');
+}
+else if (i * j < 100)
+{
+_putchar(' ');
+_putchar(i * j / 10 + '0');
+_putchar(i * j % 10 + '0');
+}
+else
+{
+_putchar(i * j / 100 + '0');
+_putchar(i * j / 10 % 10 + '0');
+_putchar(i * j % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
