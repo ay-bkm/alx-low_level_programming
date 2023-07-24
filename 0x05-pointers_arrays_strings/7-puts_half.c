86b64cb59c7d4eb1bc643f420af2120f260697a5
Ayoub <ay.belkacem.gmail.com>
2023-07-24 10:50:46 -0700

    pint half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,13 +6,13 @@
  */
 void puts_half(char *str)
 {
-	int length = strlen(str);
-	int n = (length - 1) / 2;
+	int length = strlen(str) - 1;
+	int n = (length) / 2;
 	int i = 0;
 
 	if (length % 2 != 0)
 	{
-	n = length / 2;
+	n = length + 1 / 2;
 	}
 	for (i = n; i <= length; i++)
 	{
