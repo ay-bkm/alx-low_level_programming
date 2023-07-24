e91f9884885b884c73495a6aea10b1470683b82e
Ayoub <ay.belkacem.gmail.com>
2023-07-24 09:41:47 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: character pointer
+ */
+void puts2(char *str)
+{
+	int = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	i++;
+	}
+	_putchar('\n');
+}
