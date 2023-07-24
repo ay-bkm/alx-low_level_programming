02fa823968310a760eda8e4f8dd8e3645b00a7b1
Ayoub <ay.belkacem.gmail.com>
2023-07-24 08:13:09 -0700

    Task 2 - length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - calculates the length of string
+ * @s: the string checked
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+	while (s[a] != '\0')
+	{
+		++c;
+	}
+	return (c);
+}
