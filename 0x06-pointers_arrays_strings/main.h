12a375c1c774b60be817178c822fdb38b9817c1b
Ayoub <ay.belkacem@gmail.com>
2023-07-26 05:45:05 +0100

    main header

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef FILE_MAIN
+#define FILE_MAIN
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
