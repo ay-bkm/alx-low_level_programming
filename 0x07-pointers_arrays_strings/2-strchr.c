e483c9a1587ea072d2f11bf121b8271929d5f276
Ayoub <ay.belkacem@gmail.com>
2023-07-31 19:26:47 +0100

    Task #2 - strchr: locates a character in a memory area

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strchr - locates a character in a stirng
+ *@s: string of characters
+ *@c: The character
+ *Return: pointer to c, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i]; i++)
+{
+if (s[i] == c)
+{
+return (&s[i]);
+break;
+}
+}
+return (0);
+}
