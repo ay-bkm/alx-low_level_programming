5a3e16ffdc49394a834793a1b201693309213507
Ayoub <ay.belkacem@gmail.com>
2023-08-04 14:36:04 +0100

    That's all I have -argv said.

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,12 +6,13 @@
  * @argv: argument vector
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv);
+		printf("%s\n", argv[i]);
 	}
+	return (0);
 }
