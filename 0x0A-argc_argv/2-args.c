45b839be6d11aaccd9c3002be2b4a4d2ab29f07e
Ayoub <ay.belkacem@gmail.com>
2023-08-04 14:32:50 +0100

    That's all I have -argv said.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints all the arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv);
+	}
+}
