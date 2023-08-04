333a9759936cba697f3e88c77f46d334bcf1248e
Ayoub <ay.belkacem@gmail.com>
2023-08-04 13:41:07 +0100

    Mohamed Ali Clay: What is my name?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function that prints its own name
+ * @argc: The argument count
+ * @argc: The argument vector
+ * Return: Always 0 (Success)
+ */
+int main(__attribute__((unused)) int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
