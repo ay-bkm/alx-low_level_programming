a8e5d194ce7463fc1cb7194e5321c187295ed50d
Ayoub <ay.belkacem@gmail.com>
2023-08-04 17:42:24 +0100

    The addition is real, but we are all equal

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - prints a addition of positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if argv[i] is not int, 0 Success
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j, num, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[i]);
+			sum += num;
+	}
+	printf("%d\n", sum);
+	return (2);
+}
