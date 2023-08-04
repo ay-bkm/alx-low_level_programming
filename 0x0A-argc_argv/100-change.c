2a4c8c7964aa6fa32325265710446269eb2faeb7
Ayoub <ay.belkacem@gmail.com>
2023-08-04 21:57:15 +0100

    returns change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,17 +1,17 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - gives back change
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if argv < 0, 1 if argv != 2, 2 (success)
-*/
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+
 int main(int argc, char *argv[])
 {
-	int coins[] = {25, 10, 5, 2, 1};
-	int cents, i, count;
+	int cents, ncoins = 0;
 
 	if (argc != 2)
 	{
@@ -21,23 +21,20 @@ int main(int argc, char *argv[])
 
 	cents = atoi(argv[1]);
 
-	if (cents < 0)
+	while (cents > 0)
 	{
-		printf("0\n");
-		return (0);
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
-
-	count = 0;
-	for (i = 0; coins[i]; i++)
-	{
-		while (cents >= coins[i])
-		{
-			cents -= coins[i];
-			count++;
-		}
-	}
-
-	printf("%d\n", count);
-
+	printf("%d\n", ncoins);
 	return (0);
 }
