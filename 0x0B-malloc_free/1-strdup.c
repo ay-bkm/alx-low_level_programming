3eb614e6ccaa85ed16ac07b053dbd9b6bf459727
Ayoub <ay.belkacem@gmail.com>
2023-08-08 01:26:51 +0100

    Task #1: Duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - Duplicates a string
+ * @str: the string
+ * Return: dstr, duplicate string
+ */
+char *_strdup(char *str)
+{
+	int i, s_size = 0;
+	char *dstr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		s_size++;
+
+	dstr = (char *)malloc(s_size * sizeof(char));
+	for (i = 0; i < s_size; i++)
+		dstr[i] = str[i];
+	return (dstr);
+}
