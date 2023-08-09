58135c5b8046f114d7d4eac127b5598cbcb73119
Ayoub <ay.belkacem@gmail.com>
2023-08-09 01:07:00 +0100

    Task #06: split the string in to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * word_count - counts the number of words in a string
+ * @str: the string to count
+ *
+ * Return: the number of words in the string
+ */
+int word_count(char *str)
+{
+	int idx, word_count = 0, state = 0;
+
+	for (idx = 0; str[idx]; idx++)
+	{
+		if (str[idx] == ' ')
+			state = 0;
+		else if (state == 0)
+		{
+			state = 1;
+			word_count++;
+		}
+	}
+	return (word_count);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: the string to split
+ *
+ * Return: pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+	int i, j, k, l, word_count = word_count(str);
+	char **words;
+
+	if (str == NULL || str[0] == '\0' || word_count == 0)
+		return (NULL);
+
+	words = malloc(sizeof(char *) * (word_count + 1));
+	if (words == NULL)
+		return (NULL);
+
+	for (i = 0, l = 0; i < word_count; i++)
+	{
+		while (str[l] == ' ')
+			l++;
+		for (j = l; str[j] != ' ' && str[j]; j++)
+			;
+		words[i] = malloc(sizeof(char) * (j - l + 1));
+		if (words[i] == NULL)
+		{
+			while (--i >= 0)
+				free(words[i]);
+			free(words);
+			return (NULL);
+		}
+		for (k = 0; l < j; l++, k++)
+			words[i][k] = str[l];
+		words[i][k] = '\0';
+	}
+	words[i] = NULL;
+	return (words);
+}
