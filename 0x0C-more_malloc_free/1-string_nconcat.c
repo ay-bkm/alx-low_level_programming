2c1ac649350e6fb460ea866bed40663e918d4faa
Ayoub <ay.belkacem@gmail.com>
2023-08-10 13:49:43 +0100

    Task #01: concatenates string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: number of bytes
- * Return: concatenated string
+ * string_nconcat - Concatenates two strings with a limit on the second string
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of bytes from s2 to concatenate
+ *
+ * Return: Pointer to the concatenated string, or NULL if allocation fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k;
 	char *s3;
-	unsigned int x;
 
-	s3 = NULL;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	i = 0;
 	j = 0;
-	k = 0;
+
 	while (s1[i])
 		i++;
 	while (s2[j])
@@ -30,10 +34,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s3 == NULL)
 		return (NULL);
 
-	for (x = 0; x < i; x++)
-		s3[k++] = s1[x];
-	for (x = 0; x < n; x++)
+	for (k = 0; k < i; k++)
+		s3[k] = s1[k];
+	for (unsigned int x = 0; x < n; x++)
 		s3[k++] = s2[x];
 	s3[k] = '\0';
 
+	return (s3);
 }
