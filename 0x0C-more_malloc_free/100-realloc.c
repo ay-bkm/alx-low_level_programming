33866d716c42b26feeaca86de7bc99a6617345e3
Ayoub <ay.belkacem@gmail.com>
2023-08-10 23:35:27 +0100

    Task #100: create realloc using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * _realloc - reallocated memory
  * @ptr: pointer to old memory
@@ -13,26 +13,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 
-	if (ptr == NULL)
-	{
-		new_ptr = malloc(new_size);
-		return (new_ptr);
-	}
+	if (new_size == old_size)
+		return (ptr);
 
-	if (new_size == 0)
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	if (new_size == old_size)
-		return (ptr);
-
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
 
-	memcpy(new_ptr, ptr, old_size);
+	memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
 	free(ptr);
+
 	return (new_ptr);
 }
