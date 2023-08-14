e9c257fec4b0a9bf7f3cc2ef7fd08c7885b00c24
Ayoub <ay.belkacem@gmail.com>
2023-08-14 20:48:21 +0100

    Task #00: struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog type
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+
 struct dog
 {
 	 char *name;
 	 float age;
 	 char *owner;
 };
+
+
+#endif /* DOG_H */
