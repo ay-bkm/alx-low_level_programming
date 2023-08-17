cc93a2321a25927cf27a1f49f82b2b4ca85920f7
Ayoub <ay.belkacem@gmail.com>
2023-08-18 00:39:47 +0100

    prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,46 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 /**
- * print_all - Custom print function for various data types.
- * @format: List of argument types.
- * Return: void
-*/
+ * print_all - print anything passed to it
+ * @format: The types of args passed
+ * Return: Nothing
+ */
 void print_all(const char * const format, ...)
 {
-	va_list args;
+	char *string, *separator = "";
 	int i = 0;
-	char c;
-	float f;
-	char *s;
+	va_list args;
 
 	va_start(args, format);
-	while (format && format[i])
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-				c = va_arg(args, int);
-				printf("%c", c);
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				f = va_arg(args, double);
-				printf("%f", f);
-				break;
-			case 's':
-				s = va_arg(args, char *);
-				if (s == NULL)
-					printf("(nil)");
-				else
-					printf("%s", s);
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(args, double));
+					break;
+				case 's':
+					string = va_arg(args, char *);
+					if (!string)
+						string = "(nil)";
+					printf("%s%s", separator, string);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			separator = ", ";
+			i++;
 		}
-		if (format[i + 1] != '\0'
-			&& (format[i] == 'c' || format[i] == 'i' ||
-				format[i] == 'f' || format[i] == 's'))
-			printf(", ");
-		i++;
 	}
-	va_end(args);
+
 	printf("\n");
+	va_end(args);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,9 @@
 #ifndef Variadic_Functions_H
 #define Variadic_Functions_H
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
