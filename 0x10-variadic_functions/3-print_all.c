88c95bf419d8ae50b2509a625a340d2bc5beb87b
Ayoub <ay.belkacem@gmail.com>
2023-08-17 20:31:39 +0100

    prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,31 +4,40 @@
  * @format: list of arguments
  * Return: Nothing
  */
+#include "variadic_functions.h"
+
 void print_all(const char * const format, ...)
 {
 	va_list args;
+	const char *f = NULL;
+	int count = 0;
+	char *s = NULL;
 
+	f = format;
 	va_start(args, format);
-	while (*format)
+
+	while (format && *format)
 	{
-		switch (*format)
+		if (count > 0)
+			printf(", ");
+
+		if (*format == 'c')
+			printf("%c", va_arg(args, int));
+		else if (*format == 'i')
+			printf("%d", va_arg(args, int));
+		else if (*format == 'f')
+			printf("%f", va_arg(args, double));
+		else if (*format == 's')
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				printf("%s", va_arg(args, char*) ?: "(nil)");
-				break;
+			s = va_arg(args, char*);
+			if (s)
+				printf("%s", s);
+			else
+				printf("(nil)");
 		}
 
-		if (*(++format))
-			printf(", ");
+		count++;
+		format++;
 	}
 
 	printf("\n");
