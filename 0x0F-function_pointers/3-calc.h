b6390f0800761b2b61df06fde21ef458e924bb06
Ayoub <ay.belkacem@gmail.com>
2023-08-17 02:10:57 +0100

    Task #03: a program that perform simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -5,15 +5,15 @@
 #include <stdio.h>
 
 /**
- * struct op - struct
- * @op: operator used
- * @f: function
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
-
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+    char *op;
+    int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
