2338008f6feebdceede5de3889765897ad7e484e
Ayoub <ay.belkacem@gmail.com>
2023-08-17 01:19:04 +0100

    Task #02: Find an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || array == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
@@ -21,5 +21,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
+	if (i == size)
+		return (-1);
 	return (-1);
 }
