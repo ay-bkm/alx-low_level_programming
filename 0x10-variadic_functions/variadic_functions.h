a15053c67058fb9031e7af9cdf4671a10d37c8f7
Ayoub <ay.belkacem@gmail.com>
2023-08-17 19:55:49 +0100

    Task #00: sum

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef Variadic_Functions_H
+#define Variadic_Functions_H
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif /* Variadic_Functions_H */
