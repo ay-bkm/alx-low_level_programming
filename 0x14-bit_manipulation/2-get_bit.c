f1e43e8e4bb201a15a5075cb10b324333289d750
Ayoub <ay.belkacem@gmail.com>
2023-09-01 01:39:22 +0100

    Task #02: get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,16 +4,17 @@
  * get_bit - get bit at index
  * @n: The number
  * @index: The index
- * Return: The value of the bit
+ * Return: The value of the bit, or -1 if an error occurs
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int maximum;
 
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
 	maximum = 0x01;
 	maximum <<= index;
-	if (maximum == 0)
-		return (-1);
 
 	if ((n & maximum))
 	{
