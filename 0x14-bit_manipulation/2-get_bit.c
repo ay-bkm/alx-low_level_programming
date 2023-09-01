f2a5b57f88a86400a0396ab39883bdf4be347dc5
Ayoub <ay.belkacem@gmail.com>
2023-09-01 01:35:34 +0100

    Task #02: get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - get bit at index
+ * @n: The number
+ * @index: The index
+ * Return: The value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int maximum;
+
+	maximum = 0x01;
+	maximum <<= idx;
+	if (maximum == 0)
+		return (-1);
+
+	if ((n & maximum))
+		return (1);
+	else
+		return (0);
+}
