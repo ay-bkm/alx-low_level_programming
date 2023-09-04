d71428f4dd46a995652728087377d1fa64f24384
Ayoub <ay.belkacem@gmail.com>
2023-09-05 00:04:57 +0100

    Task #01: create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,32 @@
 #include "main.h"
+
 /**
- * create_file - create file
- * @filename: pointer to the file
- * @text_content: pointer to string
- * Return: -1 or 1
+ * create_file - Creates or overwrites a file.
+ * @filename: The name of the file.
+ * @text_content: The text to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_desc;
-	ssize_t length;
+	int file_descriptor, write_status, content_length = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	file_desc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file_desc == -1)
-		return (-1);
-
-	if (text_content)
+	if (text_content != NULL)
 	{
-		length = write(file_desc, text_content, strlen(text_content));
-		if (length == -1)
-			return (-1);
+		for (content_length = 0; text_content[content_length];)
+			content_length++;
 	}
 
-	close(file_desc);
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write_status = write(file_descriptor, text_content, content_length);
+
+	if (file_descriptor == -1 || write_status == -1)
+		return (-1);
+
+	close(file_descriptor);
+
 	return (1);
 }
