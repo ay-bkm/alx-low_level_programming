7be718558c56aa0e440c1bc2d6c03179af45f046
Ayoub <ay.belkacem@gmail.com>
2023-09-04 23:21:32 +0100

    Task #00: Read and Write

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	file_desc = open(filename, "r");
+	file_desc = open(filename, O_RDONLY);
 	if (file_desc == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
