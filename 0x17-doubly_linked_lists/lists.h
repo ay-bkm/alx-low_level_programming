0d8e5696a0e791531061f560c49f116507ee769f
Ayoub <ay.belkacem@gmail.com>
2023-10-12 17:50:23 +0100

    header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -11,9 +11,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 /* Function prototypes for printing the elements, counting the elements, and adding a new node to the list */
